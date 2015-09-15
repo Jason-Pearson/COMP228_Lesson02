beb421db6735b95900c28b04f3cec780145cdd4a
Jason-Pearson <jason-pearson-1-0-7@hotmail.com>
2015-09-15 11:27:48 -0400

    Addition: static private instance variable in Program class

--- src/Program.java
+++ src/Program.java
@@ -1,9 +1,12 @@
 
 public class Program {
 
+	private static String _name = ""; //private instance variable outside main method
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Program Started...");
+		_name = "Tom"; //can't instantiate object in static main method - must initialize private static variable
+		System.out.println(_name);
 	}
 
 }
